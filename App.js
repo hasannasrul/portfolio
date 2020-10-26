25bd53bc5bd3a85d09ec7d480d7520f3f34fd162
nasrul <hasanali242424@gmail.com>
2020-10-26 17:24:03 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,19 @@
+gsap.from(".intro", {
+    opacity: 0, 
+    y: 100, 
+    duration: 1.1
+  });
+
+  gsap.from(".about-heading", {
+    opacity: 0, 
+    y: 100, 
+    duration: 1.1
+  });
+
+  gsap.from(".about-paragraph", {
+    opacity: 0, 
+    y: 100, 
+    duration: 1.1
+  });
+
+  
